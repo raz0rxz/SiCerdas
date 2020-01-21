0e5a970d0ada96c19ecc006669ad187759d50034
raz0rxz <rendibuanap@gmail.com>
2020-01-21 08:07:17 +0700

    update project

---
+++ app/src/main/java/com/rendi/aplikasisiswa/MapelActivity.java
@@ -0,0 +1,13 @@
+package com.rendi.aplikasisiswa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapel);
+    }
+}
